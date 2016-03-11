1e55d005293d807d9e57950bacdcaedf30335101
Hannu Pölönen <hannu.polonen@nosto.com>
2016-03-11 12:22:26 +0200

    Change the version name (otherwise it fails to save it to db)

--- nostotagging.php
+++ nostotagging.php
@@ -150,7 +150,7 @@ class NostoTagging extends Module
 	{
 		$this->name = 'nostotagging';
 		$this->tab = 'advertising_marketing';
-		$this->version = '3.0.0-RC2';
+		$this->version = '3.0-R2';
 		$this->author = 'Nosto';
 		$this->need_instance = 1;
 		$this->bootstrap = true;
